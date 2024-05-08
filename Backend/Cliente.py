3bbb8bca1b6b95ebba40207f777ba27bb1dac52e
Valerin Calderon <kbran1409@gmail.com>
2024-05-07 21:34:01 -0600

    Clase Cliente
    Se crea la clase cliente

---
+++ Backend/Cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, nombre, telefono, correo):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.correo = correo
+        self.mascotas = []
