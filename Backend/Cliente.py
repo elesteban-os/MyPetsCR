4ad10510d1620b8c70c2c158682e86f9e6e3516c
Valerin Calderon <kbran1409@gmail.com>
2024-05-07 21:52:32 -0600

    Update Cliente.py
    Se crea el enlace del cliente con sus mascotas

--- Backend/Cliente.py
+++ Backend/Cliente.py
@@ -4,3 +4,31 @@ class Cliente:
         self.telefono = telefono
         self.correo = correo
         self.mascotas = []
+        
+class Mascota:
+    def __init__(self, nombre, especie, raza, edad):
+        self.nombre = nombre
+        self.especie = especie
+        self.raza = raza
+        self.edad = edad
+        
+def mostrar_cliente_y_mascotas(cliente):
+    print("Cliente:", cliente.nombre)
+    print("Teléfono:", cliente.telefono)
+    print("Correo:", cliente.correo)
+    print("Mascotas:")
+    for mascota in cliente.mascotas:
+        print("- Nombre:", mascota.nombre)
+        print("  Especie:", mascota.especie)
+        print("  Raza:", mascota.raza)
+        print("  Edad:", mascota.edad)
+
+# Creación del cliente
+cliente1 = Cliente("Vale", "123456789", "Vale@gmail.com")
+
+# Agrega mascotas al cliente
+cliente1.mascotas.append(Mascota("Chloe", "Perro", "Poodle", 3))
+cliente1.mascotas.append(Mascota("Peri", "Perico", "Lora", 5))
+
+#Probar el codigo con la información asociada a x cliente 
+mostrar_cliente_y_mascotas(cliente1)
