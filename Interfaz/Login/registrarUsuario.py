9e7f897677e624332c47c433076a1d8df3071874
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-05-30 17:09:47 -0600

    Update registrarUsuario.py

--- Interfaz/Login/registrarUsuario.py
+++ Interfaz/Login/registrarUsuario.py
@@ -1,44 +1,27 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget,QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt6 import uic
-import sys 
-import re
+import sys
 import sqlite3
 
-#from Login import Login
-
-class RegisterWindow(QMainWindow):
+class FormularioRegistro(QMainWindow):
+    
     def __init__(self, parent=None):
-        super().__init__()
-        self.parent = parent
+        super().__init__(parent)
+        self.parent_window = parent
         uic.loadUi("Ventana6.ui", self)
 
-        # Conecta el botón de registro
-        self.pushButton.clicked.connect(self.register_user)
-        
-        # Conecta el botón de cancelar
-        self.pushButton_2.clicked.connect(self.close_and_return)
+       # self.pushButton.clicked.connect(self.registrar_cliente)
+        self.pushButton_2.clicked.connect(self.volver_a_pagina_administrador)
 
-    def register_user(self):
-        # Recopila los datos del formulario
-        nombre = self.lineEdit.text()
-        telefono = self.lineEdit_2.text()
-        correo = self.lineEdit_3.text()
-        contrasena = self.lineEdit_4.text()
-        mascota = self.lineEdit_5.text()
-        direccion = self.lineEdit_6.text()
-        tipo = self.lineEdit_7.text() 
-
-        # Validación simple del formulario
-        if not (nombre and telefono and correo and contrasena and mascota and direccion and tipo):
-            QMessageBox.warning(self, "Error", "Todos los campos son obligatorios")
-            return
-
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", correo):
-            QMessageBox.warning(self, "Error", "Correo electrónico no válido")
-            return
-
-
-    def close_and_return(self):
-        if self.parent is not None:
-            self.parent.show()
+    def volver_a_pagina_administrador(self):
+        if self.parent_window is not None:
+            self.parent_window.show()
         self.close()
+
+
+
+#if __name__ == "__main__":
+  #  app = QApplication(sys.argv)
+  #  window = FormularioRegistro()
+   # window.show()
+  #  sys.exit(app.exec())
