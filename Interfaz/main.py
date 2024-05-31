39e0189e8c9474662ee1712c9b858cf6acddb7fc
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-05-30 23:12:33 -0600

    Add files via upload

--- Interfaz/main.py
+++ Interfaz/main.py
@@ -1,3 +1,4 @@
+
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ventanas.Login import Login
@@ -5,16 +6,20 @@ from ventanas.registro import RegistrarUsuario
 from ventanas.cliente import ClienteWindow
 from ventanas.administrador import AdministradorWindow
 from ventanas.veterinario import VeterinarioWindow
-
+from ventanas.Inicio import Inicializar
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.inicio_window = Inicializar(self)
         self.login_window = Login(self)
         self.registro_window = RegistrarUsuario(self)
         self.cliente_window = ClienteWindow()
         self.administrador_window = AdministradorWindow()
         self.veterinario_window = VeterinarioWindow()
-        self.show_login_window()
+        self.show_inicio_window()
+
+    def show_inicio_window(self):
+        self.inicio_window.show()
 
     def show_login_window(self):
         self.login_window.show()
