2feebba2e813e6db55206e3383951041f5251a2b
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-03 12:26:33 -0600

    Update administrador.py

--- Interfaz/ventanas/administrador.py
+++ Interfaz/ventanas/administrador.py
@@ -10,7 +10,19 @@ class AdministradorWindow(QMainWindow):
         super().__init__()
         uic.loadUi("C:\Datos1_Proyecto1\MyPetsCR\Interfaz\Ventana7.ui", self)
         self.pushButton.clicked.connect(self.miau)
+        # Conectar el botón de registrar usuario a la función para abrir el formulario de registro
+        self.pushButton_1.clicked.connect(self.abrir_formulario_registro)
+        self.pushButton_9.clicked.connect(self.close_and_return)
 
+    def abrir_formulario_registro(self):
+            self.formulario_registro = RegistrarUsuario(self)
+            self.formulario_registro.show()
+
+        def close_and_return(self):
+        if self.parent() is not None:
+            self.parent().show()
+        self.close()
+            
     def miau(self):
         self.productos_window = ProductosTienda()
         self.productos_window.show()
