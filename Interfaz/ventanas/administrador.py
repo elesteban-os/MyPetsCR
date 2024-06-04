5c23bb4e5ca93f0af063c143f78c14bd4a30eacb
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-03 20:18:04 -0600

    Agregué el registro de usuario y pase la creacion de citas que antes estaba en paginaAdministrador a esta de administrado.py

--- Interfaz/ventanas/administrador.py
+++ Interfaz/ventanas/administrador.py
@@ -9,10 +9,29 @@ class AdministradorWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("C:\Datos1_Proyecto1\MyPetsCR\Interfaz\Ventana7.ui", self)
+        self.pushButton_8.clicked.connect(self.crear_cita)
+        print("pushButton_8 connected")
         self.pushButton.clicked.connect(self.miau)
+        self.pushButton_9.clicked.connect(self.close_and_return)
+        self.pushButton_1.clicked.connect(self.abrir_ventana_administrador)
+
     
+    def abrir_ventana_administrador(self):
+            self.formulario_registro = FormularioRegistro(self)
+            self.formulario_registro.show()
+    def crear_cita(self):
+        print("Crear cita")
+
+    def crear_cita(self):
+         self.crear_cita = VentanaCitasAdmi()
+         self.crear_cita.show()
+        
     def miau(self):
         self.productos_window = ProductosTienda()
         self.productos_window.show()
-
+        
+    def close_and_return(self):
+        if self.parent() is not None:
+            self.parent().show()
+        self.close() 
 
