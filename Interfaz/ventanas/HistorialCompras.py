27c1a521912a0ab555fb5f3e7532d31b03cee2cd
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-04 23:18:56 -0600

    Update HistorialCompras.py

--- Interfaz/ventanas/HistorialCompras.py
+++ Interfaz/ventanas/HistorialCompras.py
@@ -1,14 +1,12 @@
 import json
 import sys
-
 from PyQt6 import QtWidgets, uic
-from PyQt6.QtWidgets import QMessageBox, QApplication
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
-class HistorialCompras(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("C:/Datos1_Proyecto1/MyPetsCR/Interfaz/HistorialfacturacionesVeterinaria.ui", self)
+class HistorialCompras(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        uic.loadUi("C:/Datos1_Proyecto1/MyPetsCR/Interfaz/HistorialdeFacturacionesVeterinaria.ui", self)
         self.cargar_historial()
 
     def cargar_historial(self):
@@ -24,8 +22,10 @@ class HistorialCompras(QtWidgets.QWidget):
             self.tableWidget_2.setItem(row, 0, QtWidgets.QTableWidgetItem(compra['#Factura']))
             self.tableWidget_2.setItem(row, 1, QtWidgets.QTableWidgetItem(compra['Fecha']))
             self.tableWidget_2.setItem(row, 2, QtWidgets.QTableWidgetItem(compra['Cliente']))
-            self.tableWidget_2.setItem(row, 3, QtWidgets.QTableWidgetItem(compra['Productos Comprados']))
-            self.tableWidget_2.setItem(row, 4, QtWidgets.QTableWidgetItem(str(compra['Cantidad'])))
+            # Extraer los códigos de los productos comprados
+            productos_comprados = ', '.join([producto['Código'] for producto in compra['Productos Comprados']])
+            self.tableWidget_2.setItem(row, 3, QtWidgets.QTableWidgetItem(productos_comprados))
+            self.tableWidget_2.setItem(row, 4, QtWidgets.QTableWidgetItem(str(sum(producto['Cantidad'] for producto in compra['Productos Comprados']))))
             self.tableWidget_2.setItem(row, 5, QtWidgets.QTableWidgetItem(str(compra['Subtotal'])))
             self.tableWidget_2.setItem(row, 6, QtWidgets.QTableWidgetItem(str(compra['Descuento'])))
             self.tableWidget_2.setItem(row, 7, QtWidgets.QTableWidgetItem(str(compra['Total'])))
@@ -44,6 +44,7 @@ class HistorialCompras(QtWidgets.QWidget):
             json.dump(data, file, ensure_ascii=False, indent=4)
 
         self.cargar_historial()
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana_historial = HistorialCompras()
