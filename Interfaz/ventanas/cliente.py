b89e65ffa055beb5c5303e1fbf03194bb469550b
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-06 10:14:32 -0600

    Delete Interfaz directory

--- Interfaz/ventanas/cliente.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QMainWindow
-from PyQt6 import uic
-
-class ClienteWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("C:\Datos1_Proyecto1\MyPetsCR\Interfaz\Ventana5.ui", self)
-
--- Interfaz/ventanas/veterinario.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QMainWindow
-from PyQt6 import uic
-
-
-class VeterinarioWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("C:\Datos1_Proyecto1\MyPetsCR\Interfaz\Ventana8.ui", self)
