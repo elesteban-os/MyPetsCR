7472be84e659b8ce1a931fcc237b43c1edb69e9a
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-10 08:21:35 -0600

    coloque el expediente para que el cliente lo pueda ver

--- Interfaz/ventanas/cliente.py
+++ Interfaz/ventanas/cliente.py
@@ -1,8 +1,16 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6 import uic
+from .NewExpe import ExpedienteDialog
 
 class ClienteWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("C:\Datos1_Proyecto1\MyPetsCR\Interfaz\Ventana5.ui", self)
 
+        
+        self.pushButton_6.clicked.connect(self.Expediente)
+        print("pushButton_6 connected")
+
+    def Expediente(self):
+        self.expediente_mascota = ExpedienteDialog()
+        self.expediente_mascota.show()
