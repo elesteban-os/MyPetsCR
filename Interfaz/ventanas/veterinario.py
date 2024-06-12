25bad85ebb1224701b4d28030bfb47b51bffd129
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-12 16:17:54 -0600

    Coloque las funciones del veterinario.py

--- Interfaz/ventanas/veterinario.py
+++ Interfaz/ventanas/veterinario.py
@@ -1,8 +1,29 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6 import uic
-
+from .ExpedienteVet import ExpedienteDialog
+from .BuscarMascota import BuscarMascota
 
 class VeterinarioWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("C:\Datos1_Proyecto1\MyPetsCR\Interfaz\Ventana8.ui", self)
+
+        self.pushButton_6.clicked.connect(self.expedienteVeterinario)
+
+        self.pushButton_5.clicked.connect(self.close_and_return)
+
+        self.push_Button_BuscarMascota.clicked.connect(self.buscarMascota)
+
+    def buscarMascota(self):
+        self.buscar_mascota = BuscarMascota(self)
+        self.buscar_mascota.show()
+
+    def expedienteVeterinario(self):
+        self.expediente_mascota = ExpedienteDialog()
+        self.expediente_mascota.show()
+
+
+    def close_and_return(self):
+        if self.parent() is not None:
+            self.parent().show()
+        self.close()
