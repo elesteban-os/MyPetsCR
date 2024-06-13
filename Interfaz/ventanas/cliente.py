acc314bbd3414c3376304274f39b6afbcbd20f7b
MeyMora <112515882+MeyMora@users.noreply.github.com>
2024-06-12 23:06:51 -0600

    Coloque la funcion e import de crear citas para clientes.py

--- Interfaz/ventanas/cliente.py
+++ Interfaz/ventanas/cliente.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6 import uic
 from .NewExpe import ExpedienteDialog
+from .Citas_Client import MainWindow
 
 class ClienteWindow(QMainWindow):
     def __init__(self):
@@ -10,7 +11,13 @@ class ClienteWindow(QMainWindow):
         
         self.pushButton_6.clicked.connect(self.Expediente)
         print("pushButton_6 connected")
+        self.pushButton_9.clicked.connect(self.CrearCita)
+        print("pushButton_9 connected")
 
+    def CrearCita(self):
+        self.crear_cita = MainWindow()
+        self.crear_cita.show()
+        
     def Expediente(self):
         self.expediente_mascota = ExpedienteDialog()
         self.expediente_mascota.show()
